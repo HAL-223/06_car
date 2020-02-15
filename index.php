4b6180a0272c5638239b18a985eb46b2255ec954
HAL-223 <anoble.ldy8@gmail.com>
2020-02-15 23:00:14 +0900

    表示の順番について

--- index.php
+++ index.php
@@ -16,14 +16,6 @@ class Car {
   public function getName() {
     return $this->name;
   }
-  // echo $car1->getName();
-  // echo '<br>';
-  
-  
-  // 以下を実行した時にエラーがでないようにして下さい。
-  // Carクラスのインスタンスを生成
-  // 引数 : 車名、車体番号、カラー
-  
   
   // 車体番号を取得
   public function getNumber() {
@@ -51,33 +43,37 @@ class Car {
     $this->color = $color;
   }
 
-  public function infomation(){
-    echo "車の車種:" . $this->name . "車体番号:" . $this->number . "カラー:" . $this->color . "です。";
+  // echo $car1->getName();
+
+  public function selfIntroduction(){
+    echo "車の車種:" . $this->name  . "、" . "車体番号:" . $this->number . "、" . "カラー:" . $this->color . "です。" . "<br>";
   }
 }
   $car1 = new Car('ランクル', 100, 'black');
-  
-  // 車の情報表示を表示
-// echo $car1->infomation();
-// echo '<br>';
+  $car1->selfIntroduction();
 
-// // 車名(車種)を更新
-// $car1->setName('アルファード');
-// echo $car1->getName();
-// echo '<br>';
+  $car1->setName('ランクル');
+  $car1->setNumber('100');
+  $car1->setColor('black');
 
-// // 車体番号を取得
-// $car1->setNumber(200);
-// echo $car1->getNumber();
-// echo '<br>';
+  echo $car1->getName();
+  echo "<br>";
+  echo $car1->getNumber();
+  echo "<br>";
+  echo $car1->getColor();
+  echo "<br>";
 
-// // カラーを取得
-// $car1->setColor('red');
-// echo $car1->getColor();
-// echo '<br>';
 
-// // 車の情報表示を表示
-// echo $car1->infomation();
-// echo '<br>';
-// }
+  $car2 = new Car('アルファード', 200, 'red');
+  $car2->selfIntroduction();
 
+  $car1->setName('アルファード');
+  $car1->setNumber('200');
+  $car1->setColor('red');
+
+  echo $car1->getName();
+  echo "<br>";
+  echo $car1->getNumber();
+  echo "<br>";
+  echo $car1->getColor();
+  echo "<br>";
