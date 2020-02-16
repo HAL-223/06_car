f86291bb6ccb90b8d41915a6df1264f99a2126f6
HAL-223 <anoble.ldy8@gmail.com>
2020-02-16 13:34:51 +0900

    error

--- index.php
+++ index.php
@@ -11,9 +11,8 @@ class Car {
     $this->number = $number;
     $this->color = $color;
   }
-  // $car1->selfIntrodaction();
-  // 車名(車種)を取得
-  public function getName() {
+  // 車種
+    public function getName() {
     return $this->name;
   }
   
@@ -21,59 +20,132 @@ class Car {
   public function getNumber() {
     return $this->number;
   }
-  // echo $car1->getNumber();
-  // echo '<br>';
   
   // カラーを取得
   public function getColor() {
     return $this->color;
   }
-  // echo $car1->getColor();
-  // echo '<br>';
   
   public function setName($name) {
     $this->name = $name;
   }
-
+  
   public function setNumber($number) {
     $this->number = $number;
   }
-
+  
   public function setColor($color) {
     $this->color = $color;
   }
-
-  // echo $car1->getName();
-
   public function selfIntroduction(){
     echo "車の車種:" . $this->name  . "、" . "車体番号:" . $this->number . "、" . "カラー:" . $this->color . "です。" . "<br>";
+    }
+  // $car1 = new Car('ランクル', 100, 'black'); 
+  // $car1->selfIntrodaction();
+  
   }
-}
-  $car1 = new Car('ランクル', 100, 'black');
-  $car1->selfIntroduction();
+  $car1 = new Car('ランクル', 100, 'black'); 
+  $car1->selfIntrodaction();
+// 車名(車種)を取得
+$car1->name = "ランクル";
+echo $car1->getName();
+echo '<br>';
 
-  $car1->setName('ランクル');
-  $car1->setNumber('100');
-  $car1->setColor('black');
+// 車体番号を取得
+echo $car1->getNumber();
+echo '<br>';
 
-  echo $car1->getName();
-  echo "<br>";
-  echo $car1->getNumber();
-  echo "<br>";
-  echo $car1->getColor();
-  echo "<br>";
+// カラーを取得
+echo $car1->getColor();
+echo '<br>';
+
+// 車の情報表示を表示
+echo $car1->infomation();
+echo '<br>';
 
 
-  $car2 = new Car('アルファード', 200, 'red');
-  $car2->selfIntroduction();
+// 車名(車種)を更新
+$car1->setName('アルファード');
+echo $car1->getName();
+echo '<br>';
 
-  $car1->setName('アルファード');
-  $car1->setNumber('200');
-  $car1->setColor('red');
+// 車体番号を取得
+$car1->setNumber(200);
+echo $car1->getNumber();
+echo '<br>';
 
-  echo $car1->getName();
-  echo "<br>";
-  echo $car1->getNumber();
-  echo "<br>";
-  echo $car1->getColor();
-  echo "<br>";
+// カラーを取得
+$car1->setColor('red');
+echo $car1->getColor();
+echo '<br>';
+
+// 車の情報表示を表示
+echo $car1->infomation();
+echo '<br>';
+
+
+  // // $car1->selfIntrodaction();
+  // // 車名(車種)を取得
+  // public function getName() {
+  //   return $this->name;
+  // }
+  
+  // // 車体番号を取得
+  // public function getNumber() {
+  //   return $this->number;
+  // }
+  // // echo $car1->getNumber();
+  // // echo '<br>';
+  
+  // // カラーを取得
+  // public function getColor() {
+  //   return $this->color;
+  // }
+  // // echo $car1->getColor();
+  // // echo '<br>';
+  
+  // public function setName($name) {
+  //   $this->name = $name;
+  // }
+  
+  // public function setNumber($number) {
+  //   $this->number = $number;
+  // }
+  
+  // public function setColor($color) {
+  //   $this->color = $color;
+  // }
+  // public function selfIntroduction(){
+  //   echo "車の車種:" . $this->name  . "、" . "車体番号:" . $this->number . "、" . "カラー:" . $this->color . "です。" . "<br>";
+  //   }
+  // }
+  // // echo $car1->getName();
+
+  // $car1 = new Car('ランクル', 100, 'black');
+  // $car1->selfIntroduction();
+  
+  // $car1->setName('ランクル');
+  // $car1->setNumber('100');
+  // $car1->setColor('black');
+  
+  // echo $car1->getName();
+  // echo "<br>";
+  // echo $car1->getNumber();
+  // echo "<br>";
+  // echo $car1->getColor();
+  // echo "<br>";
+
+  
+  // $car2 = new Car('アルファード', 200, 'red');
+  // $car2->selfIntroduction();
+  
+  // $car1->setName('アルファード');
+  // $car1->setNumber('200');
+  // $car1->setColor('red');
+  
+  // echo $car1->getName();
+  // echo "<br>";
+  // echo $car1->getNumber();
+  // echo "<br>";
+  // echo $car1->getColor();
+  // echo "<br>";
