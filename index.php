97e9a23dd7677a781c77a6ece14903cf40670429
HAL-223 <anoble.ldy8@gmail.com>
2020-02-23 00:20:25 +0900

    エラー

--- index.php
+++ index.php
@@ -38,71 +38,28 @@ class Car {
     $this->color = $color;
   }
   public function infomation(){
-    echo "車の車種:" . $this->name  . "、" . "車体番号:" . $this->number . "、" . "カラー:" . $this->color . "、";
+    echo "車の車種:" . $this->name  . "、" . "車体番号:" . $this->number . "、" . "カラー:" . $this->color . "です。" . "<br>" . "<hr>";
   }
 }
 
 $car1 = new Car('ランクル', 100, 'black'); 
 // 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
 // 車の情報表示を表示
 echo $car1->infomation();
 echo '<br>';
 
-
-// // 車名(車種)を更新
-// $car1->setName('アルファード');
-// echo $car1->getName();
-// echo '<br>';
-
-// // 車体番号を取得
-// $car1->setNumber(200);
-// echo $car1->getNumber();
-// echo '<br>';
-
-// // カラーを取得
-// $car1->setColor('red');
-// echo $car1->getColor();
-// echo '<br>';
-
-// // 車の情報表示を表示
-// echo $car1->infomation();
-// echo '<br>';
-
 class Taxi extends Car {
   private $passeger;
-  private $pickUp;
-  private $lower;
-
-  public function __construct($passenger) {
-    $this->passenger;
-  }
-
-  public function setPassenger($passenger) {
-    $this->passenger = $passenger;
-  }
-
-  public function getPassenger() {
-    return $this->passenger;
-  }
-
+  // private $pickUp;
+  // private $lower;
   public function setPickUp($pickUp) {
     $this->pickUp = $pickUp;
   }
 
-  public function getPickUp() {
-    return $this->PickUp;
-  }
+    public function getPickUp() {
+      return $this->PickUp;
+    }
+
 
   public function setLower($lower) {
     $this->lower = $lower;
@@ -112,25 +69,25 @@ class Taxi extends Car {
     return $this->lower;
   }
 
+
   public function infomation() {
     // parent::infomation();
-    echo "車の車種:" . $this->name  . "のタクシー、" . "車体番号:" . $this->number . "、" . "カラー:" . $this->color . "、";
-    echo "乗車人数は" . $this->pickUp . "人です。";
+    echo "車の車種:" . $this->name  . "のタクシー、" . "車体番号:" . $this->number . "、" . "カラー:" . $this->color . "、" . "乗車人数は" . $this->pickUp() . "人です。";
   }
 }
 
 $taxi1 = new Taxi('クラウンのタクシー', '222', 'black');
 // 3人乗車
-$taxi1->setpickUp(3);
+$taxi1->pickUp(3);
 
 // タクシーの情報表示を表示
 echo $taxi1->infomation();
 echo '<br>';
 
 // 2人降車
-$taxi1->setlower(2);
+$taxi1->lower(2);
 echo '<br>';
 
 // 一人降車
-$taxi1->setlower(2);
+$taxi1->lower(2);
 
